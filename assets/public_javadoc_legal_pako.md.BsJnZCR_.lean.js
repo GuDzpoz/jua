128afc3ecacb67005bd2b15b3e6831414e6c055e
gudzpoz <GuDzpoz@live.com>
2024-08-28 14:01:24 +0000

    Deploying to gh-pages from @ gudzpoz/luajava@238d468c14472a9cf7f041ab09f3dad5d4197642 🚀

---
+++ assets/public_javadoc_legal_pako.md.BsJnZCR_.lean.js
@@ -0,0 +1,39 @@
+import{_ as n,o,c as a,j as e,a as t}from"./chunks/framework.SYT71_kc.js";const O=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"public/javadoc/legal/pako.md","filePath":"public/javadoc/legal/pako.md","lastUpdated":null}'),i={name:"public/javadoc/legal/pako.md"},s=e("h2",{id:"pako-v1-0",tabindex:"-1"},[t("Pako v1.0 "),e("a",{class:"header-anchor",href:"#pako-v1-0","aria-label":'Permalink to "Pako v1.0"'},"​")],-1),r=e("h3",{id:"pako-license",tabindex:"-1"},[t("Pako License "),e("a",{class:"header-anchor",href:"#pako-license","aria-label":'Permalink to "Pako License"'},"​")],-1),d=e("pre",null,`Copyright (C) 2014-2017 by Vitaly Puzrin and Andrei Tuputcyn
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+(C) 1995-2013 Jean-loup Gailly and Mark Adler
+(C) 2014-2017 Vitaly Puzrin and Andrey Tupitsin
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+claim that you wrote the original software. If you use this software
+in a product, an acknowledgment in the product documentation would be
+appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+
+`,-1),l=[s,r,d];function c(h,p,u,T,f,m){return o(),a("div",{"data-pagefind-body":!0},l)}const E=n(i,[["render",c]]);export{O as __pageData,E as default};
